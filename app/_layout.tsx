7aee2c159e7fb713ce55a30ada4ba667b0922518
Jay <jpca999@gmail.com>
2025-05-06 18:57:40 -0400

    minor changes to files positions and names

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,8 @@ import 'react-native-gesture-handler';
 export default function Layout() {
   return (
     <Drawer>
-      <Drawer.Screen name="home/index" options={{ title: 'Home' }} />
-      <Drawer.Screen name="settings/index" options={{ title: 'Settings' }} />
+      <Drawer.Screen name="home" options={{ title: 'Home' }} />
+      <Drawer.Screen name="settings" options={{ title: 'Settings' }} />
     </Drawer>
   );
 }
--- app/settings/index.js
+++ app/settings/index.js
@@ -2,10 +2,18 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-export default function ProfileScreen() {
+
+  export default function SettingsScreen() { 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile Screen</Text>
+      <Text> 1) Settigns shoudl have 
+    Ride direction filter in which direction you want to go,    </Text>
+
+    <Text>  2)  How many miles you want to pick the new ride from ?</Text>
+
+    <Text>  3)  How many miles you want to pick the new ride from ? </Text>
+
+      
     </View>
   );
 }
